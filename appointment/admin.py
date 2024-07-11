2ce4fcb7c4637b9e17e2cfce771952e73e907fca
shahidafridi-swe <shahidafridi.cse@gmail.com>
2024-07-11 21:01:35 +0600

    online appointment running email sent for join the meet

--- appointment/admin.py
+++ appointment/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Appointment
 
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
 @admin.register(Appointment)
 class AppointmentAdmin(admin.ModelAdmin):
     list_display = ['doctor_name', 'patient_name', 'appointment_type', 'appointment_status', 'created_at']
@@ -10,3 +13,13 @@ class AppointmentAdmin(admin.ModelAdmin):
 
     def patient_name(self, obj):
         return f"{obj.patient.user.first_name} {obj.patient.user.last_name}"
+
+    def save_model(self, request, obj, form, change):
+        obj.save()
+        if obj.appointment_status == "Running" and obj.appointment_type == "Online":
+            email_subject = "Your Online Appointment is Running"
+            email_body = render_to_string('online_appointment_email.html', {'user': obj.patient.user, 'doctor': obj.doctor})
+            email = EmailMultiAlternatives(email_subject,'',to=[obj.patient.user.email])
+            email.attach_alternative(email_body, 'text/html')
+            email.send()            
+    
