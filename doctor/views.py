a8ab1489b3df5978164031713f84fb1c0bb8034a
shahidafridi-swe <shahidafridi.cse@gmail.com>
2024-07-11 21:21:30 +0600

    pagination

--- doctor/views.py
+++ doctor/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, pagination
 from .models import Doctor, Designation, Specialization, AvailableTime,Review
 from .serializers import DoctorSerializer, DesignationSerializer, SpecializationSerializer,AvailableTimeSerializer,ReviewSerializer
 
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
-
+   
+    
+class DoctorPagination(pagination.PageNumberPagination):
+    page_size = 1
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+    
+    
 class DoctorViewset(viewsets.ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
+    pagination_class = DoctorPagination
+
+    
     
 class DesignationViewset(viewsets.ModelViewSet):
     # permission_classes = [IsAuthenticatedOrReadOnly]
